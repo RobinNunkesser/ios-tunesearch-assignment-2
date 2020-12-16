f6610afbbc96fec0322801743a0a924f17d29c91
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-16 23:34:06 +0100

    Initial Commit

---
+++ TuneSearchAssignment2/TuneSearchAssignment2App.swift
@@ -0,0 +1,17 @@
+//
+//  TuneSearchAssignment2App.swift
+//  TuneSearchAssignment2
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 16.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TuneSearchAssignment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
